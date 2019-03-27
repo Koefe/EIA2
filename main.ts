4fbd5965e3035aa3f33896b15a7724b5e4350b48
Felix <felix.koelle@hs-furtwangen.de>
2019-03-27 13:40:42 +0100

    Update

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", greeter);
 
 function greeter():void {
-    let promptValue = prompt("Name eingeben");
+    let promptValue : string = prompt("Name eingeben");
     console.log('Herzlich Willkommen ' + promptValue + ' Sie sehen heute super aus');
 
     document.getElementById('name').innerHTML = 'Herzlich Willkommen ' + promptValue + ' Sie sehen heute super aus';
