84d6c81ddb5c471fe2568c3c3d410f36babd1660
Felix <felix.koelle@hs-furtwangen.de>
2019-04-19 18:26:24 +0200

    Bestellungsliste funktioniert
    Bestellliste wird angezeigt. Die Slider geben allerdings nicht die richtige Zahl aus.

--- Aufgabe5/Code/main.js
+++ Aufgabe5/Code/main.js
@@ -18,22 +18,35 @@ var Eisdealer;
     let anfangsSumme = 0;
     function berechnePreis(_event) {
         anfangsSumme = 0;
+        //Damit bei jedem neuen aktualisieren die ungecheckten Checkboxen aus der Liste entnommen werden
+        document.getElementById("Liste").innerHTML = "";
         let input = document.getElementsByTagName("input");
         for (let i = 0; i < input.length; i++) {
             if (input[i].checked == true) {
-                let preis = Number(input[i].getAttribute("value"));
+                let preis = Number(input[i].value);
                 anfangsSumme += preis;
                 let bestellungsListe = document.createElement("li");
                 console.log(bestellungsListe);
-                bestellungsListe.innerHTML = `<li id="Liste">
-                <p>${input[i].className}</p>
-                </li>`;
+                bestellungsListe.innerHTML = `<p>${input[i].className}</p>`;
                 console.log(bestellungsListe);
                 document.getElementById("Liste").appendChild(bestellungsListe);
-                //if()
+                //Schreibt Preis hin
+                //let preisAnzeige: HTMLElement = document.createElement("p");
+                //preisAnzeige.innerHTML = `<p class="Preis">${anfangsSumme}</p>`;
+                //document.getElementById("Preis").appendChild(preisAnzeige);     
+            }
+            if (input[i].name == "Slider") {
+                let preis1 = Number(input[i].value);
+                anfangsSumme += preis1;
+                console.log(anfangsSumme);
+                //console.log(input[i].value);
+                if (preis1 > 0) {
+                    let bestellungsListe = document.createElement("li");
+                    bestellungsListe.innerHTML = `<p>${input[i].value} ml ${input[i].className}</p>`;
+                    document.getElementById("Liste").appendChild(bestellungsListe);
+                }
             }
         }
-        console.log(anfangsSumme);
     }
 })(Eisdealer || (Eisdealer = {}));
 //# sourceMappingURL=main.js.map
--- Aufgabe5/Code/main.ts
+++ Aufgabe5/Code/main.ts
@@ -24,25 +24,43 @@ namespace Eisdealer {
 
     function berechnePreis(_event: Event): void {
         anfangsSumme = 0;
+        //Damit bei jedem neuen aktualisieren die ungecheckten Checkboxen aus der Liste entnommen werden
+        document.getElementById("Liste").innerHTML = "";
+
         let input: HTMLCollectionOf<HTMLInputElement> = document.getElementsByTagName("input");
         for (let i: number = 0; i < input.length; i++) {
             if (input[i].checked == true) {
-                let preis: number = Number(input[i].getAttribute("value"));
+                let preis: number = Number(input[i].value);
                 anfangsSumme += preis;
 
-                
                 let bestellungsListe: HTMLLIElement = document.createElement("li");
-                console.log(bestellungsListe)
-                bestellungsListe.innerHTML = `<li id="Liste">
-                <p>${input[i].className}</p>
-                </li>`;
+                console.log(bestellungsListe);
+                bestellungsListe.innerHTML = `<p>${input[i].className}</p>`;
                 console.log(bestellungsListe);
                 document.getElementById("Liste").appendChild(bestellungsListe);
 
-                //if()
+                //Schreibt Preis hin
+                //let preisAnzeige: HTMLElement = document.createElement("p");
+                //preisAnzeige.innerHTML = `<p class="Preis">${anfangsSumme}</p>`;
+                //document.getElementById("Preis").appendChild(preisAnzeige);     
             }
+
+            if (input[i].name == "Slider") {
+                let preis1: number = Number(input[i].value);
+                anfangsSumme += preis1;
+                console.log(anfangsSumme);
+                //console.log(input[i].value);
+
+                if (preis1 > 0) {
+                    let bestellungsListe: HTMLLIElement = document.createElement("li");
+                    bestellungsListe.innerHTML = `<p>${input[i].value} ml ${input[i].className}</p>`;
+                    document.getElementById("Liste").appendChild(bestellungsListe);
+                }
+
+                
+            }
+
         }
-        console.log(anfangsSumme);
 
 
 
