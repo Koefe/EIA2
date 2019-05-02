18bff79c2f4528db7684e05f21c366fa5c8b35c9
Felix <felix.koelle@hs-furtwangen.de>
2019-05-02 12:29:38 +0200

    update

--- main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", greeter);
-function greeter() {
-    var promptValue = prompt("Name eingeben");
-    console.log('Herzlich Willkommen ' + promptValue + ' Sie sehen heute super aus');
-    document.getElementById('name').innerHTML = 'Herzlich Willkommen ' + promptValue + ' Sie sehen heute super aus';
-}
-//# sourceMappingURL=main.js.map
--- main.ts
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", greeter);
-
-function greeter():void {
-    let promptValue : string = prompt("Name eingeben");
-    console.log('Herzlich Willkommen ' + promptValue + ' Sie sehen heute super aus');
-
-    document.getElementById('name').innerHTML = 'Herzlich Willkommen ' + promptValue + ' Sie sehen heute super aus';
-}
